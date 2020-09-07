75034719c38a9560676a7928c4a1c4ae4d03e84b
He Yang <heyang0012856@gmail.com>
2020-09-07 17:35:24 +0800

    api env setting

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'//把作用域对象挂载到vue实例中，方便我们用this去调用
 import router from './router'
+import env from './env'
 
 
 //根据前端的跨域方式做调整  /api/a/b=>/a/b
 axios.defaults.baseURL='/api';
 axios.defaults.timeout=8000;
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL=env.baseURL;
 //接口错误拦截
 axios.interceptors.response.use(function(response){
   let res=response.data;
